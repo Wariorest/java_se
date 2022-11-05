6f87b41cc61edf30e1d8b40c92dde62025db68a4
Vladislav Martynov <Wariorest@gmail.com>
2022-11-05 19:46:09 +0200

    Added registry methods

--- src/singletone/Registry.java
+++ src/singletone/Registry.java
@@ -1,5 +1,29 @@
 package singletone;
 
+import java.util.ArrayList;
+
 public class Registry {
+
+    static Registry instance = null;
+    ArrayList <Employee> employers = new ArrayList<>();
+
+    public static Registry getInstance(){
+        if(instance == null){
+            instance = new Registry();
+        }
+        return instance;
+    }
+    public void addEmployeee(Employee emp)throws EmployeeRiegistryException{
+        if(employers.contains(emp)){
+            throw new EmployeeRiegistryException("Employee: "+
+                    emp.getId() + " " +
+                    emp.getFirstName() + " " +
+                    emp.getLastName() + " is exist already");
+        }else{
+            employers.add(emp);
+        }
+
+    }
+
 
 }
